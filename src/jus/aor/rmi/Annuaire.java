5a2ab0a0d6b571b214fe05d572d1ed5a984c6118
Reatha <rea.tith@gmail.com>
2016-02-19 11:09:11 +0100

    created Annuaire and Chaine classes

---
+++ src/jus/aor/rmi/Annuaire.java
@@ -0,0 +1,13 @@
+package jus.aor.rmi;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Annuaire extends UnicastRemoteObject{
+
+	protected Annuaire() throws RemoteException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/jus/aor/rmi/Chaine.java
@@ -0,0 +1,13 @@
+package jus.aor.rmi;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Chaine extends UnicastRemoteObject{
+
+	public Chaine() throws RemoteException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
