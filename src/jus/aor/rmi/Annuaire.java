39e5ea1d6ce2d35b77178f3be4d2435a37abe377
Reatha <rea.tith@gmail.com>
2016-02-19 11:12:04 +0100

    add unimplemented methods in both classes

--- src/jus/aor/rmi/Annuaire.java
+++ src/jus/aor/rmi/Annuaire.java
@@ -3,11 +3,17 @@ package jus.aor.rmi;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
-public class Annuaire extends UnicastRemoteObject{
+public class Annuaire extends UnicastRemoteObject implements _Annuaire{
 
 	protected Annuaire() throws RemoteException {
 		super();
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Numero get(String abonne) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- src/jus/aor/rmi/Chaine.java
+++ src/jus/aor/rmi/Chaine.java
@@ -2,12 +2,19 @@ package jus.aor.rmi;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.List;
 
-public class Chaine extends UnicastRemoteObject{
+public class Chaine extends UnicastRemoteObject implements _Chaine {
 
 	public Chaine() throws RemoteException {
 		super();
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public List<Hotel> get(String localisation) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
