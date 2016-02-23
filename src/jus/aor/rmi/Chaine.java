0e6b38c2e004807141f53c5d7d26d153b7748aba
Marwan Hallal <mhallal@users.noreply.github.com>
2016-02-23 19:08:51 +0200

    Implemented a basic version of RMI client

--- src/jus/aor/rmi/Chaine.java
+++ src/jus/aor/rmi/Chaine.java
@@ -8,13 +8,12 @@ public class Chaine extends UnicastRemoteObject implements _Chaine {
 
 	public Chaine() throws RemoteException {
 		super();
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	@Override
 	public List<Hotel> get(String localisation) {
-		// TODO Auto-generated method stub
-		return null;
+		
 	}
 
 }
--- src/jus/aor/rmi/LookForHotel.java
+++ src/jus/aor/rmi/LookForHotel.java
@@ -2,7 +2,15 @@ package jus.aor.rmi; /**
  * J<i>ava</i> U<i>tilities</i> for S<i>tudents</i>
  */
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 /**
  * Représente un client effectuant une requête lui permettant d'obtenir les numéros de téléphone des hôtels répondant à son critère de choix.
@@ -12,6 +20,7 @@ public class LookForHotel{
 
 	/** le critère de localisaton choisi */
 	private String localisation;
+	
 
 	// ...
 	/**
@@ -29,7 +38,34 @@ public class LookForHotel{
 	 * @throws RemoteException
 	 */
 	public long call() throws RemoteException {
-		// ...
+		long start = System.nanoTime();
+		List<Hotel> hotels = new ArrayList<>();
+		Map<Hotel, Numero> ann = new HashMap<>();
+
+		String[] chaines;
+		
+		try {
+			// Découvrir toutes les chaines exposées par le registre et interroger chacune.
+			chaines = Naming.list("//localhost/");
+			for (String ch : chaines) {
+				if(Pattern.matches("//*/Chaine*", ch)) {
+					_Chaine c = (_Chaine) Naming.lookup("ch");
+					hotels.addAll(c.get(localisation));
+				}
+			}
+			
+			_Annuaire annuaire = (_Annuaire) Naming.lookup("//localhost/Annuaire");
+			for (Hotel hotel : hotels) {
+				Numero num = annuaire.get(hotel.name);
+				ann.put(hotel, num);
+			}
+			
+		} catch (MalformedURLException | NotBoundException e) {
+			e.printStackTrace();
+		}
+		long end = System.nanoTime();
+		
+		return end - start;
 	}
 
 	// ...
