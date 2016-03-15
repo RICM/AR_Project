f23f810470b1d83c9f2ecb95cee6a1935f746e96
Marwan Hallal <marwanh@users.noreply.github.com>
2016-03-15 23:02:55 +0100

    implement AgentServer

--- src/jus/aor/mobilagent/kernel/AgentServer.java
+++ src/jus/aor/mobilagent/kernel/AgentServer.java
@@ -3,8 +3,14 @@
  */
 package jus.aor.mobilagent.kernel;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.UnknownHostException;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -12,33 +18,76 @@ import java.util.Map;
  *
  */
 public class AgentServer {
-	
-	Map<String, _Service<?>> services;
-	
-	public AgentServer(int i, String s) {}
-	
-	public void run() {}
-	
+
+	private Map<String, _Service<?>> services;
+	private String name;
+	private int port;
+
+	public AgentServer(int p, String n) {
+		port = p;
+		name = n;
+		services = new HashMap<String, _Service<?>>();
+	}
+
+	public void run() {
+		ServerSocket servSoc = null;
+		try {
+			servSoc = new ServerSocket(port);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+		Socket clientSoc;
+		BAMAgentClassLoader agentLoader = new BAMAgentClassLoader(this.getClass().getClassLoader());
+		while (true) {
+			try {
+				clientSoc = servSoc.accept();
+				// get the mobile agent
+				AgentInputStream ais = new AgentInputStream(clientSoc.getInputStream(), agentLoader);
+				_Agent agent = (_Agent) ais.readObject();
+				// get the repository
+				Jar jar = (Jar) ais.readObject();
+				agentLoader.integrateCode(jar);
+				agent.init(this, name);
+				new Thread(agent).start();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+		}
+
+	}
+
 	public void addService(String name, _Service<?> service) {
 		services.put(name, service);
 	}
-	
-	public String toString() {return super.toString();};
-	
+
+	public String toString() {
+		return name + ":" + port;
+	};
+
 	public _Service<?> getService(String name) {
 		return services.get(name);
 	}
-	
-	public URI site() {return null;}
-	
-	private _Agent getAgent(Socket s) {return null;}
-	
-	
-	
-	
-	
-	
-	
-	
+
+	public URI site() {
+		URI uri = null;
+		try {
+			uri = new URI("mobilagent", null, InetAddress.getLocalHost().getHostName(), port, null, null, null);
+		} catch (URISyntaxException | UnknownHostException e) {
+		}
+		return uri;
+	}
+
+	private _Agent getAgent(Socket soc) throws IOException, ClassNotFoundException {
+		AgentInputStream ais = new AgentInputStream(soc.getInputStream(),
+				new BAMAgentClassLoader(this.getClass().getClassLoader()));
+		return (_Agent) ais.readObject();
+	}
 
 }
