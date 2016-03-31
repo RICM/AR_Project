adb782d9c63c0feb7481ec65c020e9465dbee543
Marwan Hallal <marwanh@users.noreply.github.com>
2016-04-01 01:37:09 +0200

    fix - call to Annuaire service returns a single number, not a collection.

--- src/jus/aor/mobilagent/hostel/Annuaire.java
+++ src/jus/aor/mobilagent/hostel/Annuaire.java
@@ -1,21 +1,54 @@
-/**
- * 
- */
 package jus.aor.mobilagent.hostel;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import jus.aor.mobilagent.kernel._Service;
 
-/**
- * @author Marwan Hallal
- *
- */
-public class Annuaire implements _Service<Map<String, Numero>> {
-	
+public class Annuaire implements _Service<Numero> {
+
+	private Map<String, Numero> annuaire;
+
+	public Annuaire(Object... args) {
+		String xmlFile = (String) args[0];
+		annuaire = loadDirectory(xmlFile);
+	}
+
 	@Override
-	public Map<String, Numero> call(Object... params) throws IllegalArgumentException {
-		return null;
+	public Numero call(Object... params) throws IllegalArgumentException {
+		if (params.length != 1)
+			throw new IllegalArgumentException();
+		String abonne = (String) params[0];
+		return annuaire.get(abonne);
+	}
+
+	private Map<String, Numero> loadDirectory(String file) {
+		Map<String, Numero> annuaire = new HashMap<>();
+		try {
+			DocumentBuilder docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			Document doc = docBuilder.parse(new File(file));
+			NodeList hotelNodes = doc.getElementsByTagName("Telephone");
+			for (int i = 0; i < hotelNodes.getLength(); i++) {
+				NamedNodeMap m = hotelNodes.item(i).getAttributes();
+				String name = m.getNamedItem("name").getNodeValue();
+				String numero = m.getNamedItem("numero").getNodeValue();
+				annuaire.put(name, new Numero(numero));
+			}
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			e.printStackTrace();
+		}
+		return annuaire;
 	}
 
 }
