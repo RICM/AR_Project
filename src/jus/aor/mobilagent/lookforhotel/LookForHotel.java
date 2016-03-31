fe50d4a31f126b4da922f329d13eb2ed58856d83
Marwan Hallal <marwanh@users.noreply.github.com>
2016-03-31 23:51:42 +0200

    create LookForHotel agent

---
+++ src/jus/aor/mobilagent/lookforhotel/LookForHotel.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package jus.aor.mobilagent.lookforhotel;
+
+import jus.aor.mobilagent.kernel.Agent;
+import jus.aor.mobilagent.kernel._Action;
+
+public class LookForHotel extends Agent {
+
+	private static final long serialVersionUID = -4912085058159815502L;
+
+	@Override
+	protected _Action retour() {
+		return null;
+	}
+
+}
